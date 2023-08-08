9a0a8a28f997dd12007cc06f833869feef782bde
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-08-08 22:47:30 +0100

    Fixed python scripts for API advanced tasks

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,35 +2,34 @@
 """This module queries the reddit API"""
 import requests
 
-def count_words(subreddit, word_list, after=None, counts=None):
+def count_words(subreddit, word_list, after=None, counts={}):
     """This function queries the reddit API recursively
     """
-    if counts is None:
-        counts = {}
-
-    if after is None:
-        url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-    else:
-        url = f"https://www.reddit.com/r/{subreddit}/hot/.json?after={after}"
-    
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     headers = {"User-Agent": "Python/requests"}
-    params = {"limit": 100}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    params = {
+        "after": after,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     
-    if response.status_code == 200:
-        data = response.json().get("data")
-        after = data.get("after")
+    if response.status_code != 200:
+        return
+    
+    results = response.json().get("data")
+    after = results.get("after")
+    
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower()
+        for word in word_list:
+            word = word.lower()
+            if word in title and " " + word + " " not in title:
+                counts[word] = counts.get(word, 0) + 1
 
-        for child in data.get("children"):
-            title = child.get("data").get("title").lower()
-            for word in word_list:
-                word = word.lower()
-                if word in title and f" {word} " not in title:
-                    counts[word] = counts.get(word, 0) + 1
-
-        if after is not None:
-            return count_words(subreddit, word_list, after, counts)
-        else:
-            sorted_counts = sorted(counts.items(), key=lambda item: (-item[1], item[0]))
-            for word, count in sorted_counts:
-                print(f"{word}: {count}")
+    if after is not None:
+        return count_words(subreddit, word_list, after, counts)
+    
+    sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+    for word, count in sorted_counts:
+        print(f"{word}: {count}")
